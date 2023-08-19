fc8959353fc4668fc30e3b4509b354717cd419f1
j0rdanaaa <j0rdanaaa@gmail.com>
2023-08-19 08:55:21 -0300

    template autenticação em react

--- src/components/CriarPost/CriarPost.js
+++ src/components/CriarPost/CriarPost.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import { FormPost, Input, TextArea } from './styled'
+import React from "react";
+import { FormPost, Input, TextArea } from "./styled";
+import { BASE_URL } from "../../constants/BASE_URL";
+import axios from "axios";
+import useForms from "../../hooks/useForms";
 
 export default function CriarPost() {
+  const { form, onChange, limparCampos } = useForms(
+    { titulo: "",
+    post: "" });
 
   const enviarPost = (e) => {
-    console.log("entrou")
-  }
+    e.preventDefault();
+    console.log("entrou");
 
+    const token = localStorage.getItem("token");
+    const body = {
+      title: form.titulo,
+      body: form.post,
+    };
+
+    const headers = {
+      headers: {
+        Authorization: token,
+      },
+    };
+
+    axios
+      .post(`${BASE_URL}/posts`, body, headers)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <FormPost onSubmit={enviarPost}>
-      <label htmlFor='tituloPost'>Título:</label>
-      <Input placeholder='digite um título para o seu post' />
-      <label htmlFor='textoPost'>Texto:</label>
-      <TextArea placeholder='crie um post!' />
+      <label htmlFor="tituloPost">Título:</label>
+      <Input
+        name="titulo"
+        value={form.titulo}
+        onChange={onChange}
+        placeholder="digite um título para o seu post"
+      />
+      <label htmlFor="textoPost">Texto:</label>
+      <TextArea
+        name="post"
+        value={form.post}
+        onChange={onChange}
+        placeholder="crie um post!"
+      />
       <button>Enviar</button>
     </FormPost>
-  )
+  );
 }
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import CardPost from '../../components/CardPost/CardPost'
-import CriarPost from '../../components/CriarPost/CriarPost'
-import useRequestData from '../../hooks/useRequestData'
-import { FeedContainer } from './styled'
-
+import React, { useEffect } from "react";
+import CardPost from "../../components/CardPost/CardPost";
+import CriarPost from "../../components/CriarPost/CriarPost";
+import useRequestData from "../../hooks/useRequestData";
+import { FeedContainer } from "./styled";
+import { irParaLogin } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
+import { useProtectedpage } from "../../hooks/useProtectedPage";
 
 export default function Feed() {
-  const [posts] = useRequestData([], '/posts')
+  useProtectedpage();
+  const tokenLogado = localStorage.getItem("token");
+
+  const config = {
+    headers: {
+      Authorization: tokenLogado,
+    },
+  };
+
+  const [posts] = useRequestData([], "/posts", config);
 
   return (
     <FeedContainer>
@@ -15,11 +26,9 @@ export default function Feed() {
         <h3>Novo post</h3>
         <CriarPost />
       </section>
-      {
-        posts.map((post) => {
-          return <CardPost key={post.id} post={post} />
-        })
-      }
+      {posts.map((post) => {
+        return <CardPost key={post.id} post={post} />;
+      })}
     </FeedContainer>
-  )
+  );
 }
